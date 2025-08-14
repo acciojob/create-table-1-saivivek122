316b2fca4c30700a78df62145f2bf13eea1fdb7f
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-14 07:24:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+let table=document.getElementById("sampleTable");
+
+
 function insert_Row() {
     //Write your code here
+	let row=document.createElement("tr");
+	let td1=document.createElement("td");
+	td1.textContent="New Cell1";
+	let td2=document.createElement("td");
+	td2.textContent="New Cell2";
+	row.appendChild(td1);
+	row.appendChild(td2);
+	table.insertBefore(row,table.firstChild)
+
   
   
 }
